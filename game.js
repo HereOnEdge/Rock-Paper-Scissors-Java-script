8b322bb1bde9949edba3787338f75c62d20fd134
Reza <reza1377maleki@gmail.com>
2022-09-03 18:21:35 +0430

    user can choose in window prompt and game will anounce if user won or lost

--- game.js
+++ game.js
@@ -1,15 +1,35 @@
 // rock paper scissors game where you play against computer.
-// ask for an input inside window prompt
-// computer checks if input is correct
-/*if input is not correct raise an error 
-in window alert*/ 
 
-/* computer picks one item randomly
-set 3 variables for each item and put their
+/*set 3 variables for each item and put their
 value as their name(rock,paper,...) as s string */
 const rock = "Rock";
 const paper = "Paper";
 const sciss = "Scissors";
+
+/* create a function that ask for a number
+between 1 to 3 as an input inside window prompt 
+*/
+Choose = () => {
+    let userChoice = Number(window.prompt("chosse. 1 = rock, 2 = paper, 3 = scissors,"))
+    /*if input is not correct raise an error 
+    in window alert*/ 
+    if (userChoice > 3 || userChoice < 1){
+        return window.alert('choose between 1,2,3')
+    }
+    //  if input is correct use switch to convert it into text
+    else {switch(userChoice){
+        case 1 :
+            return rock;
+            break;
+        case 2 :
+            return paper;
+            break;
+        case 3 :
+            return sciss;        
+    }}
+}
+
+// computer picks one item randomly
 
 /* create a function called "getComputerChoice" 
 pick a random round number from 1 to 3
@@ -34,7 +54,34 @@ getComputerChoice = () => {
     }
 
 }
-console.log(getComputerChoice());
+// create three variables for win , loose and tie
+const win = "You Won";
+const loose = "You Loose";
+const tie = "Tie"
 
-// show computer's choice inside window alert
+// create another function that has two properties to decide the winner
+pickWinner = (playerSelection , computerSelection) =>{
+
+    // compare computerSelection and playerSelection
+    if (computerSelection == rock && playerSelection == paper){
+        return window.alert(`${win} Paper beats Rock`);
+    } else if (computerSelection == rock && playerSelection == sciss){
+        return window.alert(`${loose} Rock beats Scissors`);
+    } else if (computerSelection == rock && playerSelection == rock){
+        return window.alert(`${tie} Rock and Rock are tie`);
+    } else if (computerSelection == paper && playerSelection == rock){
+        return window.alert(`${loose} Paper beats Rock`);
+    } else if (computerSelection == paper && playerSelection == paper){
+        return window.alert(`${tie} Paper and Paper are tie`);
+    } else if ( computerSelection == paper && playerSelection == sciss){
+        return window.alert(`${win} Scissors beats Paper`);
+    } else if (computerSelection == sciss && playerSelection == rock){
+        return window.alert(`${win} Rock beats Scissors`);
+    } else if (computerSelection == sciss && playerSelection == paper){
+        return window.alert(`${loose} Scissors beats Paper`);
+    } else if (computerSelection == sciss && playerSelection == sciss){
+        return window.alert(`${tie} Scissors and Scissors are tie`)
+    }
+}
+pickWinner(Choose(),getComputerChoice());
 // show the winner. 
