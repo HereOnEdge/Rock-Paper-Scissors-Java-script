c279063985fe9b0bc999bc38e8546042408dfdca
Reza <reza1377maleki@gmail.com>
2022-09-03 16:36:09 +0430

    Update README and add algorithm for computer choice

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Rock-Paper-Scissors-Java-script
+rock paper scissors game where you play against computer.
+we ask for an input inside window prompt and after user has given an input , computer checks if input is correct then picks one item randomly and show it inside window alert beside the winner. 
+
+#Rock-Paper-Scissors-Java-script
---
+++ game.js
@@ -0,0 +1,17 @@
+// rock paper scissors game where you play against computer.
+// ask for an input inside window prompt
+// computer checks if input is correct
+/*if input is not correct raise an error 
+in window alert*/ 
+/* computer picks one item randomly
+set 3 variables for each item and put their
+value as their name(rock,paper,...) as s string */
+/* pick a random round number from 1 to 3
+using floor and math and save it's result 
+inside a variable called "picked" */
+/* using a switch statement check the
+"picked" value and return "Rock" if
+it's 1 , return "Paper" if it's 2 , and return
+"Scissors" if it's 3. */
+// show computer's choice inside window alert
+// show the winner. 
