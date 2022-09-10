2df4d852be0b967386d2c4ab7590f259bd5594e2
Reza <reza1377maleki@gmail.com>
2022-09-10 16:46:33 +0430

    add UI to game for single round

--- game.js
+++ game.js
@@ -5,28 +5,69 @@ value as their name(rock,paper,...) as s string */
 const rock = "Rock";
 const paper = "Paper";
 const sciss = "Scissors";
+let userChoice = '';
+const buttonContainer = document.querySelector(".buttons");
+const label = document.querySelector('label');
+const bestOf = document.querySelector(".best-of");
+const rock_btn = document.querySelector('.rock');
+const paper_btn = document.querySelector('.paper');
+const sciss_btn = document.querySelector('.scissors');
+const start_btn = document.querySelector('.start');
+const ask = document.createElement('h2');
+const winner = document.createElement('p');
+const computerChoice = document.createElement("p")
 
+
+
+start_btn.addEventListener('click', () => {
+    if (bestOf.value == ""){
+        bestOf.focus()
+    } else {
+        const container = document.querySelector(".container")
+        const resultDiv = document.createElement('div');
+        container.insertBefore(resultDiv, buttonContainer)
+        resultDiv.appendChild(winner);
+        resultDiv.appendChild(ask);
+        resultDiv.appendChild(computerChoice)
+        computerChoice.classList.add("computer-choice")
+        ask.classList.add("ask");
+        resultDiv.classList.add("result");
+        ask.textContent = "Choose between one of the buttons below"
+        buttonContainer.style = "display: flex";
+        start_btn.style.display = "none";
+        label.style.display = "none";
+    }
+    
+})
+rock_btn.onclick = () => {
+    ask.style.display = "none";
+    userChoice = rock;
+    pickWinner(userChoice, getComputerChoice())
+};
+paper_btn.onclick = () => {
+    ask.style.display = "none";
+    userChoice = paper;
+    pickWinner(userChoice, getComputerChoice())
+};
+sciss_btn.onclick = () => {
+    ask.style.display = "none";
+    userChoice = sciss;
+    pickWinner(userChoice, getComputerChoice())
+};
 /* create a function that ask for a number
 between 1 to 3 as an input inside window prompt 
 */
 Choose = () => {
-    let userChoice = Number(window.prompt("chosse. 1 = rock, 2 = paper, 3 = scissors,"))
-    /*if input is not correct raise an error 
-    in window alert*/ 
-    if (userChoice > 3 || userChoice < 1){
-        return window.alert('choose between 1,2,3')
-    }
-    //  if input is correct use switch to convert it into text
-    else {switch(userChoice){
-        case 1 :
-            return rock;
-            break;
-        case 2 :
-            return paper;
-            break;
-        case 3 :
-            return sciss;        
-    }}
+    rock_btn.onclick = () => {
+        userChoice = rock;
+        pickWinner(userChoice, getComputerChoice())
+        console.log(userChoice)
+    };
+    paper_btn.onclick = () => userChoice = paper;
+    sciss_btn.onclick= () => userChoice = sciss;
+    // let userChoice = Number(window.prompt("chosse. 1 = rock, 2 = paper, 3 = scissors,"))
+    
+    return userChoice;
 }
 
 // computer picks one item randomly
@@ -44,51 +85,72 @@ getComputerChoice = () => {
     picked = Math.floor((Math.random() * 3) + 1);
     switch(picked){
         case 1 :
+            computerChoice.textContent = `Computer choosed ${rock}`;
             return rock;
             break;
         case 2 :
+            computerChoice.textContent = `Computer choosed ${paper}`;
             return paper;
             break;
         case 3 :
+            computerChoice.textContent = `Computer choosed ${sciss}`;
             return sciss;        
     }
 
 }
 // create three variables for win , loose and tie
-const win = "You Won";
-const loose = "You Loose";
-const tie = "Tie"
+const win = "You WON! ";
+const loose = "You LOOSE! ";
+const tie = "TIE! "
 
 // create another function that has two properties to decide the winner
 pickWinner = (playerSelection , computerSelection) => {
 
     // compare computerSelection and playerSelection
     if (computerSelection == rock && playerSelection == paper){
-        window.alert(`${win} Paper beats Rock`);
+        winner.classList.add("winner");
+        winner.textContent = `${win} Paper beats Rock`;
+        winner.style.cssText = "display: block; color: #03ad14";
         return win;
     } else if (computerSelection == rock && playerSelection == sciss){
-        window.alert(`${loose} Rock beats Scissors`);
+        winner.classList.add("winner");
+        winner.textContent = `${loose} Rock beats Scissors`;
+        winner.style.cssText = "display: block; color: #ee2603";
         return loose;
     } else if (computerSelection == rock && playerSelection == rock){
-        window.alert(`${tie} Rock and Rock are tie`);
+        winner.classList.add("winner");
+        winner.textContent = `${tie} Rock and Rock are tie`;
+        winner.style.cssText = "display: block; color: #f5f1f0";
         return tie;
     } else if (computerSelection == paper && playerSelection == rock){
-        window.alert(`${loose} Paper beats Rock`);
+        winner.classList.add("winner"); 
+        winner.textContent = `${loose} Paper beats Rock`;
+        winner.style.cssText = "display: block; color: #ee2603";
         return loose;
     } else if (computerSelection == paper && playerSelection == paper){
-        window.alert(`${tie} Paper and Paper are tie`);
+        winner.classList.add("winner");
+        winner.textContent = `${tie} Paper and Paper are tie`;
+        winner.style.cssText = "display: block; color: #f5f1f0";
         return tie;
     } else if ( computerSelection == paper && playerSelection == sciss){
-        window.alert(`${win} Scissors beats Paper`);
+        winner.classList.add("winner");
+        winner.textContent = `${win} Scissors beats Paper`;
+        winner.style.cssText = "display: block; color: #03ad14";
         return win;
     } else if (computerSelection == sciss && playerSelection == rock){
-        window.alert(`${win} Rock beats Scissors`);
+        winner.classList.add("winner");
+        winner.textContent = `${win} Rock beats Scissors`;
+        winner.style.cssText = "display: block; color: #03ad14";
         return win;
     } else if (computerSelection == sciss && playerSelection == paper){
-        window.alert(`${loose} Scissors beats Paper`);
+        winner.classList.add("winner");
+        winner.textContent = `${loose} Scissors beats Paper`;
+        winner.style.cssText = "display: block; color: #ee2603";
         return loose;
     } else if (computerSelection == sciss && playerSelection == sciss){
-        window.alert(`${tie} Scissors and Scissors are tie`)
+        winner.classList.add("winner");
+        winner.textContent = `${tie} Scissors and Scissors are tie`;
+        winner.style.cssText = "display: block; color: #f5f1f0";
         return tie;
     }
 }
@@ -124,4 +186,4 @@ game = (rounds) => {
     }
 }
 // run the game
-game(5)
+// game(5)
